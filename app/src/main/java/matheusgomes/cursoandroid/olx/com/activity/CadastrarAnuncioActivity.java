dc5dce1ffec1372477d04088fe046f18315c52dd
Matheus <matheusgomez2013@gmail.com>
2022-08-02 15:33:48 -0300

    Interface de cadastro de anúncio. Máscaras de moeda e telefone. Configurando permissões.

--- app/src/main/java/matheusgomes/cursoandroid/olx/com/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/matheusgomes/cursoandroid/olx/com/activity/CadastrarAnuncioActivity.java
@@ -1,27 +1,41 @@
 package matheusgomes.cursoandroid.olx.com.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import matheusgomes.cursoandroid.olx.com.R;
 import matheusgomes.cursoandroid.olx.com.databinding.ActivityCadastrarAnuncioBinding;
 import matheusgomes.cursoandroid.olx.com.helper.Permissoes;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity
+implements View.OnClickListener {
 
     private ActivityCadastrarAnuncioBinding binding;
 
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,34 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
         //Validar permissões
         Permissoes.validarPermissoes( permissoes, this, 1 );
+
+        binding.imageCadastro1.setOnClickListener( this );
+        binding.imageCadastro2.setOnClickListener( this );
+        binding.imageCadastro3.setOnClickListener( this );
+
+        carregarDadosSpinner();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch ( view.getId() ){
+
+            case R.id.imageCadastro1 :
+                escolherImagem( 1 );
+                break;
+
+            case R.id.imageCadastro2 :
+                escolherImagem( 2 );
+                break;
+
+            case R.id.imageCadastro3 :
+                escolherImagem( 3 );
+                break;
+
+
+        }
 
     }
 
@@ -79,4 +121,53 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == Activity.RESULT_OK){
+
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configura imagem no ImageView
+            if( requestCode == 1 ){
+                binding.imageCadastro1.setImageURI( imagemSelecionada );
+            }else if( requestCode == 2 ){
+                binding.imageCadastro2.setImageURI( imagemSelecionada );
+            }else if( requestCode == 3 ){
+                binding.imageCadastro3.setImageURI( imagemSelecionada );
+            }
+
+            listaFotosRecuperadas.add( caminhoImagem );
+
+        }
+
+    }
+
+    private void carregarDadosSpinner(){
+
+        String[] estados = getResources().getStringArray( R.array.estados );
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                estados
+        );
+        adapter.setDropDownViewResource( com.google.android.material.R.layout.support_simple_spinner_dropdown_item );
+        binding.spinnerEstado.setAdapter( adapter );
+
+        String[] categorias = getResources().getStringArray( R.array.categoria );
+        ArrayAdapter<String> adapterCategoria = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                categorias
+        );
+        adapterCategoria.setDropDownViewResource( com.google.android.material.R.layout.support_simple_spinner_dropdown_item );
+        binding.spinnerCategoria.setAdapter( adapterCategoria );
+
+    }
 }
