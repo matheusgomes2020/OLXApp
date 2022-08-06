73d5702c72b0b0b6f8b954f8fd702727a4d898a4
Matheus <matheusgomez2013@gmail.com>
2022-08-06 15:44:09 -0300

    Aplicar filtros por categoria. Exibindo detalhes do produto.

--- app/src/main/java/matheusgomes/cursoandroid/olx/com/activity/DetalhesProdutoActivity.java
+++ app/src/main/java/matheusgomes/cursoandroid/olx/com/activity/DetalhesProdutoActivity.java
@@ -2,20 +2,76 @@ package matheusgomes.cursoandroid.olx.com.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import matheusgomes.cursoandroid.olx.com.R;
 import matheusgomes.cursoandroid.olx.com.databinding.ActivityDetalhesProdutoBinding;
+import matheusgomes.cursoandroid.olx.com.model.Anuncio;
 
 public class DetalhesProdutoActivity extends AppCompatActivity {
 
-    private ActivityDetalhesProdutoBinding binding;
+    private CarouselView carouselView;
+    private TextView titulo;
+    private TextView descricao;
+    private TextView estado;
+    private TextView preco;
+    private Anuncio anuncioSelecionado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produto);
 
-        binding = ActivityDetalhesProdutoBinding.inflate( getLayoutInflater() );
-        setContentView( binding.getRoot() );
+        //Configurar toolbar
+        getSupportActionBar().setTitle("Detalhe produto");
+
+        //Incializar componentes de interface
+        inicializarComponentes();
+
+        //Recupera anúncio para exibicao
+        anuncioSelecionado = (Anuncio) getIntent().getSerializableExtra("anuncioSelecionado");
+
+        if( anuncioSelecionado != null ){
+
+            titulo.setText( anuncioSelecionado.getTitulo() );
+            descricao.setText( anuncioSelecionado.getDescricao() );
+            estado.setText( anuncioSelecionado.getEstado() );
+            preco.setText( anuncioSelecionado.getValor());
+
+            ImageListener imageListener = new ImageListener() {
+                @Override
+                public void setImageForPosition(int position, ImageView imageView) {
+                    String urlString = anuncioSelecionado.getFotos().get( position );
+                    Picasso.get().load(urlString).into(imageView);
+                }
+            };
+
+            carouselView.setPageCount( anuncioSelecionado.getFotos().size() );
+            carouselView.setImageListener( imageListener );
+
+        }
+
     }
+    public void visualizarTelefone(View view){
+        Intent i = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", anuncioSelecionado.getTelefone(), null ));
+        startActivity( i );
+    }
+
+    private void inicializarComponentes(){
+        carouselView = findViewById(R.id.carouselView2);
+        titulo = findViewById(R.id.textTituloDetalhe);
+        descricao = findViewById(R.id.textDescricaoDetalhe);
+        estado = findViewById(R.id.textEstadoDetalhe);
+        preco = findViewById(R.id.textPrecoDetalhe);
+    }
+
 }
